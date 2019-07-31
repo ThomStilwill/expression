ed14ac2186ee58c21b760bb2f75b750a403849b1
ThomStilwill <thom.stilwill@gmail.com>
2019-07-31 18:13:16 -0400

    expression tree implemented

--- expression.js
+++ expression.js
@@ -1,37 +1,221 @@
-var Tuple = /** @class */ (function () {
-    function Tuple() {
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+//abstract base-class
+var GraphNode = /** @class */ (function () {
+    function GraphNode() {
     }
-    return Tuple;
+    GraphNode.prototype.compute = function (context) { throw new Error("not implemented"); };
+    GraphNode.prototype.toString = function () { throw new Error("not implemented"); };
+    return GraphNode;
 }());
-function firstToken(expression, position) {
-    var ops = ['=', '>', '<'];
-    var nextposition = expression.length;
-    var nextOperator = '';
-    ops.forEach(function (op) {
-        var newposition = expression.indexOf(op, position);
-        if (newposition === -1) {
+//leaf-nodes
+var ValueNode = /** @class */ (function (_super) {
+    __extends(ValueNode, _super);
+    function ValueNode(value) {
+        var _this = _super.call(this) || this;
+        _this.value = value;
+        return _this;
+    }
+    ValueNode.prototype.compute = function () { return this.value; };
+    ValueNode.prototype.toString = function () { return JSON.stringify(this.value); };
+    return ValueNode;
+}(GraphNode));
+var PropertyNode = /** @class */ (function (_super) {
+    __extends(PropertyNode, _super);
+    function PropertyNode(property) {
+        var _this = _super.call(this) || this;
+        _this.property = property;
+        return _this;
+    }
+    PropertyNode.prototype.compute = function (context) { return context[this.property]; };
+    PropertyNode.prototype.toString = function () { return String(this.property); };
+    return PropertyNode;
+}(GraphNode));
+//tree-nodes
+var UnaryNode = /** @class */ (function (_super) {
+    __extends(UnaryNode, _super);
+    function UnaryNode(op, node) {
+        var _this = this;
+        if (!(node instanceof GraphNode)) {
+            throw new Error("invalid node passed");
         }
-        else {
-            if (newposition < nextposition) {
-                nextposition = newposition;
-            }
-            nextOperator = op;
+        _this = _super.call(this) || this;
+        _this.op = op;
+        _this.node = node;
+        return _this;
+    }
+    UnaryNode.prototype.compute = function (context) {
+        var v = this.node.compute(context);
+        switch (this.op) {
+            case "NOT": return !v;
+        }
+        throw new Error("operator not implemented '" + this.op + "'");
+    };
+    UnaryNode.prototype.toString = function () {
+        return "( " + this.op + " " + this.node.toString() + " )";
+    };
+    UnaryNode.operators = ["NOT"];
+    return UnaryNode;
+}(GraphNode));
+var BinaryNode = /** @class */ (function (_super) {
+    __extends(BinaryNode, _super);
+    function BinaryNode(op, l, r) {
+        var _this = this;
+        if (!(l instanceof GraphNode && r instanceof GraphNode)) {
+            throw new Error("invalid node passed");
+        }
+        _this = _super.call(this) || this;
+        _this.op = op;
+        _this.left = l;
+        _this.right = r;
+        return _this;
+    }
+    BinaryNode.prototype.compute = function (context) {
+        var l = this.left.compute(context);
+        var r = this.right.compute(context);
+        switch (this.op) {
+            //logic operators
+            case "AND": return l && r;
+            case "OR": return l || r;
+            //comparison-operators
+            case "=": return l === r;
+            case "<=": return l <= r;
+            case ">=": return l >= r;
+            case "!=": return l != r;
+            case ">": return l > r;
+            case "<": return l < r;
+            //computational operators
+            case "+": return l + r;
+            case "-": return l - r;
+            case "*": return l * r;
+            case "/": return l / r;
+        }
+        throw new Error("operator not implemented '" + this.op + "'");
+    };
+    BinaryNode.prototype.toString = function () {
+        return "( " + this.left.toString() + " " + this.op + " " + this.right.toString() + " )";
+    };
+    BinaryNode.operators = [
+        "*", "/", "+", "-",
+        ">", "<", "<=", ">=", "!=", "=",
+        "AND", "OR",
+    ];
+    return BinaryNode;
+}(GraphNode));
+//dot is kind of special:
+var DotNode = /** @class */ (function (_super) {
+    __extends(DotNode, _super);
+    function DotNode(l, r) {
+        var _this = this;
+        /*
+        if(!(l instanceof PropertyNode || l instanceof DotNode)){
+            throw new Error("invalid left node")
+        }
+        */
+        if (!(r instanceof PropertyNode)) {
+            throw new Error("invalid right node");
+        }
+        _this = _super.call(this, ".", l, r) || this;
+        return _this;
+    }
+    DotNode.prototype.compute = function (context) {
+        //especially because of this composition:
+        //fetch the right property in the context of the left result
+        return this.right.compute(this.left.compute(context));
+    };
+    DotNode.prototype.toString = function () {
+        return this.left.toString() + "." + this.right.toString();
+    };
+    return DotNode;
+}(BinaryNode));
+function escapeForRegex(str) {
+    return String(str).replace(/[.*+?^=!:${}()|[\]\/\\]/g, '\\$&');
+}
+//dynamically build my parsing regex:
+var tokenParser = new RegExp([
+    //numbers
+    /\d+(?:\.\d*)?|\.\d+/.source,
+    //string-literal
+    //  /["](?:\\[\s\S]|[^"])+["]|['](?:\\[\s\S]|[^'])+[']/.source,
+    //booleans
+    //"true|false",
+    //operators
+    [".", "(", ")"].concat(UnaryNode.operators, BinaryNode.operators)
+        .sort(function (a, b) { return b.length - a.length; }) //so that ">=" is added before "=" and ">", for example
+        .map(escapeForRegex)
+        .join("|"),
+    //properties
+    //has to be after the operators
+    /[a-zA-Z$_][a-zA-Z0-9$_]*/.source,
+    //remaining (non-whitespace-)chars, just in case
+    //has to be at the end
+    /\S/.source
+].map(function (s) { return "(" + s + ")"; }).join("|"), "g");
+function parse(str) {
+    var tokens = [];
+    //abusing str.replace() as a RegExp.forEach
+    str.replace(tokenParser, function (token, number, op, property) {
+        if (number) {
+            token = new ValueNode(+number);
+        }
+        else if (property) {
+            token = new PropertyNode(property);
+        }
+        else if (!op) {
+            throw new Error("unexpected token '" + token + "'");
+        }
+        tokens.push(token);
+    });
+    for (var i; (i = tokens.indexOf(".")) > -1;) {
+        tokens.splice(i - 1, 3, new DotNode(tokens[i - 1], tokens[i + 1]));
+    }
+    for (var i, j; (i = tokens.lastIndexOf("(")) > -1 && (j = tokens.indexOf(")", i)) > -1;) {
+        tokens.splice(i, j + 1 - i, process(tokens.slice(i + 1, j)));
+    }
+    if (~tokens.indexOf("(") || ~tokens.indexOf(")")) {
+        throw new Error("mismatching brackets");
+    }
+    return process(tokens);
+}
+function process(tokens) {
+    UnaryNode.operators.forEach(function (token) {
+        for (var i = -i; (i = tokens.indexOf(token, i + 1)) > -1;) {
+            tokens.splice(i, 2, new UnaryNode(token, tokens[i + 1]));
         }
     });
-    var tuple = new Tuple();
-    tuple.value = expression.substring(position, nextposition);
-    tuple.operator = nextOperator;
-    tuple.nextposition = nextposition + nextOperator.length;
-    return tuple;
-}
-function tokenize(expression) {
-    var position = -1;
-    while (position < expression.length) {
-        var tuple = firstToken(expression, position);
-        position = tuple.nextposition;
-        console.log(tuple.value, tuple.operator);
+    BinaryNode.operators.forEach(function (token) {
+        for (var i = 1; (i = tokens.indexOf(token, i - 1)) > -1;) {
+            tokens.splice(i - 1, 3, new BinaryNode(token, tokens[i - 1], tokens[i + 1]));
+        }
+    });
+    if (tokens.length !== 1) {
+        console.log("error: ", tokens.slice());
+        throw new Error("something went wrong");
     }
-    return "Hello, " + expression;
+    return tokens[0];
 }
-console.log(tokenize('name=bill'));
+var data = {
+    id: 12345,
+    a: { source: 12345 },
+    target: 12345,
+    color: "#FF0",
+    blue: "#00F",
+    age: 20
+};
+var tree = parse("((a.source = id)AND (target= id) AND ( NOT( color != blue) OR ( age<= 23 )))");
+//var tree = parse("1=1=age+10>30"); //to test operator precedence
+console.log(tree.compute(data));
+console.log(tree.toString());
+// console.log(JSON.stringify(tree, null, 2));
 //# sourceMappingURL=expression.js.map
--- expression.ts
+++ expression.ts
@@ -1,50 +1,227 @@
+// Source: https://stackoverflow.com/questions/37469768/infix-to-binary-expression-tree
 
-class Tuple {
-    value: string;
-    operator: string;
-    nextposition: number;
+//abstract base-class
+class ExpressionNode {
+    compute(context){ throw new Error("not implemented") }
+    toString(){ throw new Error("not implemented") }
 }
 
+//leaf-nodes
+class ValueNode extends ExpressionNode{
+    public value: any;
+    constructor(value){ 
+        super();
+        this.value = value; 
+    }
+    compute(){ return this.value; }
+    toString(){ return JSON.stringify(this.value); }
+}
 
-function firstToken(expression,position) {
+class PropertyNode extends ExpressionNode{
+    public property: any;
+    constructor(property){
+        super();
+        this.property = property;
+    }
+    compute(context){ return context[this.property]; }
+    toString(){ return String(this.property); }
+}
 
-    const ops = ['=', '>', '<'];
-    let nextposition = expression.length;
-    let nextOperator = '';
-    
-    ops.forEach(op => {
-        const newposition = expression.indexOf(op,position);
-        if(newposition === -1){
+//tree-nodes
+class UnaryNode extends ExpressionNode{
 
-        } else {
- 
-            if(newposition < nextposition) {
-                nextposition = newposition;
-            }
-            nextOperator = op;
+    public static operators = ["NOT"];
+    public op: string;
+    public node: ExpressionNode;
+
+    constructor(op, node){
+        if(!(node instanceof ExpressionNode)){
+            throw new Error("invalid node passed")
         }
-    });
-    
-    const tuple = new Tuple();
-    tuple.value = expression.substring(position,nextposition);
-    tuple.operator = nextOperator;
-    tuple.nextposition = nextposition + nextOperator.length;
-
-    return tuple;
+        super();
+        this.op = op;
+        this.node = node;
+    }
+    compute(context){
+        var v:any = this.node.compute(context);
+        switch(this.op){
+            case "NOT": return !v;
+        }
+        throw new Error("operator not implemented '"+this.op+"'");
+    }
+    toString(){
+        return  "( " + this.op + " " + this.node.toString() + " )";
+    }   
 }
 
-function tokenize(expression) {
 
-    let position = -1;
+class BinaryNode extends ExpressionNode{
 
-    while (position < expression.length) {
+    public static  operators = [
+        "*","/","+","-",
+        ">","<","<=",">=","!=","=",
+        "AND","OR",
+    ]
 
-        let tuple = firstToken(expression,position);
-        position = tuple.nextposition;
-        console.log(tuple.value, tuple.operator);
+    public left: ExpressionNode;
+    public op: string;
+    public right: ExpressionNode;
+
+    constructor(op:any, l:any, r:any){
+        if(!(l instanceof ExpressionNode && r instanceof ExpressionNode)){
+            throw new Error("invalid node passed")
+        }
+        super();
+        this.op = op;
+        this.left = l;
+        this.right = r;
     }
 
-    return "Hello, " + expression;
+    compute(context){
+        let l:any = this.left.compute(context);
+        let r:any = this.right.compute(context);
+        switch(this.op){
+            //logic operators
+            case "AND": return l && r;
+            case "OR": return l || r;
+
+            //comparison-operators
+            case "=": return l === r;
+            case "<=": return l <= r;
+            case ">=": return l >= r;
+            case "!=": return l != r;
+            case ">": return l > r;
+            case "<": return l < r;
+
+            //computational operators
+            case "+": return l + r;
+            case "-": return l - r;
+            case "*": return l * r;
+            case "/": return l / r;
+        }
+        throw new Error("operator not implemented '"+ this.op + "'");
+    }
+
+    toString(){
+        return "( " + this.left.toString() + " " + this.op + " " + this.right.toString() + " )";
+    }
 }
 
-console.log(tokenize('name=bill'));
+//dot is kind of special:
+class DotNode extends BinaryNode{
+    constructor(l:any, r:any){
+        /*
+        if(!(l instanceof PropertyNode || l instanceof DotNode)){
+            throw new Error("invalid left node")
+        }
+        */
+        if(!(r instanceof PropertyNode)){
+            throw new Error("invalid right node")
+        }
+        super(".", l, r);
+    }
+
+    compute(context){
+        //especially because of this composition:
+        //fetch the right property in the context of the left result
+        return this.right.compute( this.left.compute(context) );
+    }
+    toString(){
+        return this.left.toString() + "." + this.right.toString();
+    }
+}
+
+function escapeForRegex(str){
+    return String(str).replace(/[.*+?^=!:${}()|[\]\/\\]/g, '\\$&');
+}
+
+//dynamically build my parsing regex:
+var tokenParser = new RegExp([
+        //numbers
+        /\d+(?:\.\d*)?|\.\d+/.source,
+
+        //string-literal
+        //  /["](?:\\[\s\S]|[^"])+["]|['](?:\\[\s\S]|[^'])+[']/.source,
+
+        //booleans
+        //"true|false",
+
+        //operators
+        [".", "(", ")"].concat(UnaryNode.operators, BinaryNode.operators)
+            .sort((a,b) => b.length-a.length) //so that ">=" is added before "=" and ">", for example
+            .map(escapeForRegex)
+            .join("|"),
+
+        //properties
+        //has to be after the operators
+        /[a-zA-Z$_][a-zA-Z0-9$_]*/.source,
+
+        //remaining (non-whitespace-)chars, just in case
+        //has to be at the end
+        /\S/.source
+    ].map(s => "("+ s +")").join("|"), "g");
+
+function parse(str){
+    var tokens = [];
+    //abusing str.replace() as a RegExp.forEach
+    str.replace(tokenParser, function(token, number, op, property){
+        if(number){
+            token = new ValueNode(+number);
+        }else if(property){
+            token = new PropertyNode(property);
+        }else if(!op){
+            throw new Error("unexpected token '"+token+"'");
+        }
+        tokens.push(token);
+    });
+
+    for(var i; (i=tokens.indexOf(".")) > -1; ){
+        tokens.splice(i-1, 3, new DotNode(tokens[i-1], tokens[i+1]))
+    }
+
+    for(var i,j; (i=tokens.lastIndexOf("(")) > -1 && (j=tokens.indexOf(")", i)) > -1;){
+        tokens.splice(i, j+1-i, process(tokens.slice(i+1, j)));
+    }
+    if(~tokens.indexOf("(") || ~tokens.indexOf(")")){
+        throw new Error("mismatching brackets");
+    }
+
+    return process(tokens);
+}
+
+function process(tokens){
+    UnaryNode.operators.forEach(token => {
+        for(var i=-i; (i=tokens.indexOf(token, i+1)) > -1;){
+            tokens.splice(i, 2, new UnaryNode(token, tokens[i+1]));
+        }
+    })
+
+    BinaryNode.operators.forEach(token => {
+        for(var i=1; (i=tokens.indexOf(token, i-1)) > -1;){
+            tokens.splice(i-1, 3, new BinaryNode(token, tokens[i-1], tokens[i+1]));
+        }
+    });
+
+    if(tokens.length !== 1){
+        console.log("error: ", tokens.slice());
+        throw new Error("something went wrong");
+    }
+    return tokens[0];
+}
+
+var data = {
+    id: 12345,
+    a: { source: 12345 },
+    target: 12345,
+    color: "#FF0",
+    blue: "#00F",
+    age: 20
+}
+
+var expression = "((a.source = id)AND (target= id) AND ( NOT( color != blue) OR ( age<= 23 )))";
+var tree = parse(expression);
+//var tree = parse("1=1=age+10>30"); //to test operator precedence
+
+console.log(tree.compute(data));
+console.log(tree.toString());
+// console.log(JSON.stringify(tree, null, 2));
